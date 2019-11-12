d957604d07f653cf1b607d435dac77858eb7df15
Alejandro Bolano <elbola.cuba1992@gmail.com>
2019-11-12 19:21:34 +0100

    [module-3] Copy excersice 3.3 of module 3

---
+++ CIPSA-Master-CSharp/CIPSA-CSharp-Module3-3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIPSA_CSharp_Module3_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A continuación sumará dos números introducidos:");
+            Console.WriteLine("Introduzca el primer número");
+            var firstNumber = Console.ReadLine();
+            Console.WriteLine("Introduzca el segundo número");
+            var secondNumber = Console.ReadLine();
+            Console.WriteLine("El resultado es: " + (Convert.ToInt32(firstNumber) + Convert.ToInt32(secondNumber)));
+        }
+    }
+}

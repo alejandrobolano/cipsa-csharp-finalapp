4589b215aa06656f75590182a6ed625b26bbb556
Alejandro Bolano <elbola.cuba1992@gmail.com>
2019-11-12 19:15:02 +0100

    [module-3] Copy excersice 3.1 of module 3

---
+++ CIPSA-Master-CSharp/CIPSA-CSharp-Module3-3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIPSA_CSharp_Module3_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phraseOne = "Creando frases básicas de muestra para ejercicio";
+            var phraseTwo = "Está bien celebrar el éxito, pero es más importante aprender las lecciones del fracaso";
+            var phraseThree = "Para ser grande, a veces tienes que correr grandes riesgos";
+
+            Console.WriteLine($" {phraseOne}" +
+                              $"\n {phraseTwo}" +
+                              $"\n {phraseThree}");
+            Console.ReadLine();
+        }
+    }
+}

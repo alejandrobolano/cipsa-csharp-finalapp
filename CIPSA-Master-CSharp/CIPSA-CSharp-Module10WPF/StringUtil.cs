7cb3be6fdef6deb5345fde002aff4e053a25f3d9
Alejandro <elbola.cuba1992@gmail.com>
2020-03-29 21:41:10 +0200

    [module-10] Add wpf of module 10

---
+++ CIPSA-Master-CSharp/CIPSA-CSharp-Module10WPF/StringUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIPSA_CSharp_Module10WPF
+{
+    class StringUtil
+    {
+        public static string ConcatPhrases(string phrase1, string phrase2, bool showWhiteSpace)
+        {
+            return showWhiteSpace ? phrase1 + " " + phrase2 : phrase1 + phrase2;
+        }
+
+        public static string SearchAndDeleteWord(string phrase, string word)
+        {
+            if (!IsExistWord(phrase, word))
+            {
+                throw new Exception($"La palabra {word} no existe");
+            }
+            return phrase.Replace(word,string.Empty);
+        }
+
+
+        private static bool IsExistWord(string phrase, string word)
+        {
+            return phrase.Contains(word);
+        }
+    }
+}

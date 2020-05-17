dc57f4458447361eaea2f7abadf0ffe60d77dac6
Alejandro <elbola.cuba1992@gmail.com>
2020-05-17 14:46:18 +0200

    Add State to Rental

---
+++ CIPSA-CSharp.Net-Final-App/VideoClub.Common.Model/Enums/StateRentalEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace VideoClub.Common.Model.Enums
+{
+    public enum StateRentalEnum
+    {
+        [Description("Activa")]
+        Activated,
+        [Description("Devuelta")]
+        Returned,
+        All
+    }
+}
---
+++ CIPSA-CSharp.Net-Final-App/VideoClub.Infrastructure.Repository/Migrations/202005171150369_AddStateRental.cs
@@ -0,0 +1,18 @@
+﻿namespace VideoClub.Infrastructure.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddStateRental : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Rentals", "State", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Rentals", "State");
+        }
+    }
+}

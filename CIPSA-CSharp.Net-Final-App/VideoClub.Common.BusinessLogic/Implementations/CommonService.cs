385d544646970afbdd8e807807af1d87080aad1d
Alejandro <elbola.cuba1992@gmail.com>
2020-05-18 17:31:40 +0200

    Add GetProduct(string) into new CommonService ´s class

---
+++ CIPSA-CSharp.Net-Final-App/VideoClub.Common.BusinessLogic/Implementations/CommonService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoClub.Common.BusinessLogic.Dto;
+using VideoClub.Common.Model.Utils;
+
+namespace VideoClub.Common.BusinessLogic.Implementations
+{
+    public class CommonService
+    {
+        public static ProductDto GetProduct(string productId)
+        {
+            ProductDto product;
+            if (productId.Contains(CommonHelper.Movie))
+            {
+                product = MovieService.Instance.Get(productId);
+            }
+            else
+            {
+                product = VideoGameService.Instance.Get(productId);
+            }
+
+            return product;
+        }
+    }
+}
